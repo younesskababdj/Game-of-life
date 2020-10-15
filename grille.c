df253c5ebc08394896f61279630d5615de28dc46
kabbadj <kabbadj@uds-501617.ad.unistra.fr>
2020-10-15 18:41:27 +0200

    Cration du makefile et programmation des fonctions alloue_grille et libere_grille

--- grille.c
+++ grille.c
@@ -1,5 +1,25 @@
 #include "grille.h"
 
+void alloue_grille (int l, int c, grille* g){
+	int i, j;
+	g->nbl = l;
+	g->nbc = c;
+	g->cellules = (int**) malloc(l * sizeof(int*));
+
+	for (i = 0; i < l; i++)
+	{
+		g->cellules[i] = (int*) malloc(c * sizeof(int));
+			for (j = 0; j < c; j++)
+			{
+			set_morte(i, j, *g);
+			}
+	}
+}
+
+void libere_grille (grille* g){
+	free(g);
+}
+
 void init_grille_from_file (char * filename, grille* g){
 	FILE * pfile = NULL;
 	pfile = fopen(filename, "r");
@@ -13,7 +33,7 @@ void init_grille_from_file (char * filename, grille* g){
 	alloue_grille(l,c,g);
 	
 	fscanf(pfile, "%d", & vivantes);
-	for (n=0; n< vivantes; ++n){
+	for (n=0; n < vivantes; ++n){
 		fscanf(pfile, "%d", & i);
 		fscanf(pfile, "%d", & j);
 		set_vivante(i,j,*g);
@@ -26,6 +46,9 @@ void init_grille_from_file (char * filename, grille* g){
 
 void copie_grille (grille gs, grille gd){
 	int i, j;
-	for (i=0; i<gs.nbl; ++i) for (j=0; j<gs.nbc; ++j) gd.cellules[i][j] = gs.cellules[i][j];
+	for (i=0; i<gs.nbl; ++i)
+	{
+		for (j=0; j<gs.nbc; ++j) gd.cellules[i][j] = gs.cellules[i][j];
+	}
 	return;	
 }
---
+++ makefile
@@ -0,0 +1,33 @@
+#Le compilateur utilisé 
+CC = gcc
+
+
+#Les Flags de compilation 
+CFLAGS = -I$(INC_DIR) -Wall
+
+INC_DIR = include
+BIN_DIR = bin
+OBJ_DIR = obj
+SRC_DIR = src
+DOC_DIR = doxyfile
+
+
+main: main.o jeu.o io.o grille.o
+	@mkdir -p $(BIN_DIR)
+	$(CC) $(CFLAGS) -o $(BIN_DIR)/$@ $(OBJ_DIR)/main.o $(OBJ_DIR)/jeu.o $(OBJ_DIR)/io.o $(OBJ_DIR)/grille.o
+
+
+%.o: %.c
+	@mkdir -p $(OBJ_DIR)
+	$(CC) $(CFLAGS) -o $(OBJ_DIR)/$@ -c $<
+
+
+#La cible dist pour créer le tar 
+dist:
+	@mkdir -p dist
+	tar -J -cvf dist/KabbadjYouness.tar.xz  include src makefile DOC_DIR 
+
+#La cible clean pour la supression des fichiers inutiles 
+clean:
+	rm -f $(OBJ_DIR)/*.o
+	rm -f $(BIN_DIR)/*
